ff99fa50d3912d5ae0798c2ef2107c320d128f6a
Oradell35 <122868692+Odarell35@users.noreply.github.com>
2024-08-13 16:16:44 +0200

    test case added

--- src/test/java/com/example/taxcalculator/TaxcalculatorApplicationTests.java
+++ src/test/java/com/example/taxcalculator/TaxcalculatorApplicationTests.java
@@ -1,7 +1,10 @@
 package com.example.taxcalculator;
 
+import com.example.taxcalculator.service.TaxService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class TaxcalculatorApplicationTests {
@@ -10,4 +13,9 @@ class TaxcalculatorApplicationTests {
 	void contextLoads() {
 	}
 
+	@Test
+	void TestcalculateTax() {
+		TaxService taxService = new TaxService();
+		assertEquals(4081.34,taxService.calculateTaxAge(495000, 23,0, 0));
+	}
 }
